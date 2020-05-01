35e21b739fd318e1e2ea3918a3517480d0e6ffe5
ThomasHenriksen11 <61276429+ThomasHenriksen11@users.noreply.github.com>
2020-05-01 13:59:37 +0200

    Tilføjet klasser samt mapper til at holde styr på de forskellige ting
    Arbejder videre på at kunne gemme en værdi i en variable at brug af en knap i WPF

---
+++ PipelineSystem/Domain Layer/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineSystem
+{
+    class Customer
+    {
+        public string CompanyName;
+        public string CVR;
+        public string Email;
+    }
+}
---
+++ PipelineSystem/Domain Layer/Deal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineSystem
+{
+    class Deal
+    {
+        private string dealName;
+        public string DealName
+        {
+            get
+            {
+                return name1;
+            }
+            set
+            {
+                name1 = value;
+                Textbox.content = name1;
+            }
+        }
+        
+        public string ContactName;
+        public int Value;
+    }
+}
