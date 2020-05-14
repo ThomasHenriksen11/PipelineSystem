d55c5a356d543797b7508526f32f35ed20ededd2
Thomas Dall Henriksen <thom43q1@edu.ucl.dk>
2020-05-14 09:35:35 +0200

    Rigtige problem

--- PipelineSystem/test.xaml.cs
+++ PipelineSystem/test.xaml.cs
@@ -17,11 +17,54 @@ namespace PipelineSystem
     /// <summary>
     /// Interaction logic for test.xaml
     /// </summary>
-    public partial class test : Window
-    {
-        public test()
-        {
-            InitializeComponent();
-        }
-    }
+    //public partial class test : Window
+    //{
+      //  public test()
+        //{
+          //  InitializeComponent();
+        //}
+        //private void List_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        //{
+            // Store the mouse position
+          //  startPoint = e.GetPosition(null);
+        //}
+        //private void List_MouseMove(object sender, MouseEventArgs e)
+        //{
+            // Get the current mouse position
+          //  Point mousePos = e.GetPosition(null);
+            //Vector diff = startPoint - mousePos;
+
+            //if (e.LeftButton == MouseButtonState.Pressed &&
+              //  Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                //Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+            //{
+                // Get the dragged ListViewItem
+              //  ListView listView = sender as ListView;
+               // ListViewItem listViewItem =
+                 //   FindAnchestor<ListViewItem>((DependencyObject)e.OriginalSource);
+
+                // Find the data behind the ListViewItem
+                //Contact contact = (Contact)listView.ItemContainerGenerator.
+                  //  ItemFromContainer(listViewItem);
+
+                // Initialize the drag & drop operation
+                //DataObject dragData = new DataObject("myFormat", contact);
+                //DragDrop.DoDragDrop(listViewItem, dragData, DragDropEffects.Move);
+            //}
+        //}
+        //private static T FindAnchestor<T>(DependencyObject current)
+    //where T : DependencyObject
+        //{
+          //  do
+            //{
+              //  if (current is T)
+                //{
+                  //  return (T)current;
+                //}
+                //current = VisualTreeHelper.GetParent(current);
+            //}
+            //while (current != null);
+            //return null;
+        //}
+    //}
 }
