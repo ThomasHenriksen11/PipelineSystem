586e09696a34daf1cb378e31f292de314f37c724
Thomas Dall Henriksen <thom43q1@edu.ucl.dk>
2020-07-14 10:55:33 +0200

    Tilføjet remove funktion(nu mangler vi bare noget vi kan remove). Tilføjet tilbage knapper

--- PipelineSystem/Domain Layer/Pipeline.cs
+++ PipelineSystem/Domain Layer/Pipeline.cs
@@ -10,5 +10,10 @@ namespace PipelineSystem.Domain_Layer
     {
         public string PipelineName { get; set; }
 
+        public override string ToString()
+        {
+            return PipelineName;
+        }
+
     } 
 }
